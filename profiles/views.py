ed7f429ce4c28ac7baf710fd6ed76bb6d0d43afa
HMuraja <hilla.muraja@hotmail.com>
2023-09-25 22:34:22 +0000

    Modify profiles app Add more fields to the ProfileList. Add more fields to the ProfileDetail Add the fields to the profileserializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django.db.models import Count
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -9,7 +10,11 @@ class ProfileList(generics.ListAPIView):
     Using ListAPIView as using get method only
     profile methods are created when new user signsup.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        reviews_count=Count('owner__review', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 
@@ -18,6 +23,10 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Generic view RetrieveUpdateAPIView, retrieves and
     edits the profile fetched.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        reviews_count=Count('owner__review', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
