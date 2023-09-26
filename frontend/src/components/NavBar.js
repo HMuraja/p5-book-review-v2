e2a9ee8849023cdfd5794a1ea3a33e01eb916f76
HMuraja <hilla.muraja@hotmail.com>
2023-09-26 20:32:54 +0000

    Styling Add google fonts for body and links. Add color for hovering. Add background color and content color. Add other styles for future.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,20 +1,41 @@
 import React from "react";
 import logo from "../assets/rr-logo.png";
-import {Nav, Navbar, Container} from 'react-bootstrap';
+import {Nav, Navbar, Container, NavLink} from 'react-bootstrap';
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return(
-  <Navbar bg="light" expand="lg">
+  <Navbar 
+    className={styles.NavBar}
+    expand="md"
+    fixed="top"
+  >
     <Container>
-        <Navbar.Brand href="/">
-            <img src={logo} alt="logo" height="80" />
-        </Navbar.Brand>
+        <NavLink>
+            <Navbar.Brand href="/">
+                <img src={logo} alt="logo" height="80" />
+            </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto text-left">
-            <Nav.Link href="#home" className="mr-auto text-left">
-                <i className="fas fa-home"></i> Home
-            </Nav.Link>
+            <Nav className="ml-auto text-left">
+                <NavLink 
+                    href="#home" 
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}>
+                    <i className="fas fa-home"></i> Home
+                </NavLink>
+                <NavLink 
+                    href="#home" 
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}>
+                    <i className="fas fa-sign-in-alt"></i> Sign In
+                </NavLink>
+                <NavLink href="#home" 
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}>
+                    <i className="fas fa-user-plus"></i> Sign Up
+                </NavLink>
             </Nav>
         </Navbar.Collapse>
     </Container>
