4a0d4309b78032e3ba037a043dffac92286f9a6d
HMuraja <hilla.muraja@hotmail.com>
2023-09-27 11:07:04 +0000

    SignUp from Create signup form in pages folder inside src. Add form from react bootsrap and style it by adding Button and SignUp css. Add Handle Change to the input fields. Add handleSubmit to submit form data. Succesful submission and registration to be implemented in the next commit. Api folder added to src. Set up axios data and  interceptors.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -22,8 +24,37 @@ function App() {
           <Route
               exact
               path="/signup"
-              render={() => <h1> Sign Up </h1>}
+              render={() => <SignUpForm/>}
             />
+          <Route
+              exact
+              path="/signout"
+              render={() => <h1> Signout </h1>}
+            />
+          <Route
+              exact
+              path="/reviews/reviews/create"
+              render={() => <h1> Write review !</h1>}
+            />
+          <Route
+              exact
+              path="/reviews/:id"
+              render={() => <h1> View review details! </h1>}
+            />
+          <Route
+              exact
+              path="/reviews/:id/edit"
+              render={() => <h1> Edit review! </h1>}
+          />
+          <Route
+              exact
+              path="/reviews/profiles/edit"
+              render={() => <h1> Edit profile </h1>}
+          />
+          <Route
+              render={() => <p>Page not found!</p>}
+            />
+          
         </Switch>
       </Container>
     </div>
